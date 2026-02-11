9c77dd626c6e8057c8c0f12b42a775fef7da079d
Sachin Kumar <sachin.codes01@gmail.com>
2026-02-11 17:10:47 +0530

    add the code

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import Home from './pages/Home/Home.jsx'
+import Login from './pages/Login/Login.jsx'
+import Player from './pages/Player/Player.jsx'
+import { Routes, Route, useNavigate } from 'react-router-dom'
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from './firebase.js'
+import { ToastContainer, toast } from 'react-toastify';
+
+function App() {
+
+  const navigate = useNavigate();
+
+  useEffect(()=>{
+    onAuthStateChanged(auth, async (user)=>{
+      if(user){
+        console.log('Logged In')
+        navigate('/')
+      }else{
+        console.log('Logged Out')
+        navigate('/login')
+      }
+    })
+  },[])
+  return (
+    <div>
+        <ToastContainer theme='dark' />
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/player/:id' element={<Player/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
